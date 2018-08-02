419876de7a41d0a054448cdeefe4d3cc277b31a1
Chris Bailey <me@cbailey.co.uk>
2018-08-02 23:40:10 +0100

    Added code for potential configuration via sys.config file, need to test

--- src/wacko_app.erl
+++ src/wacko_app.erl
@@ -15,10 +15,27 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    io:format("Controller Dir: ~p~n", [filename:join([code:priv_dir(wacko), "controllers"])]),
-    code:add_path(filename:join([code:priv_dir(wacko), "controllers"])),
+    init_wacko(),
     wacko_sup:start_link().
 
 stop(_State) ->
     ok.
 
+%% Initialise and print out useful information
+init_wacko() ->
+    Env = application:get_all_env(wacko),
+    set_controller_dir(Env),
+    code:add_path(filename:join([code:priv_dir(wacko), "controllers"])).
+
+set_controller_dir(Env) ->
+    Opt = case lists:keyfind(controller_dir, 1, Env) of
+              false   -> default;
+              {_K, V} -> V
+          end,
+    
+    Dir = case filelib:is_dir(Opt) of
+              true  -> Opt;
+              false -> filename:join([code:priv_dir(wacko), "controllers"])
+          end,
+
+    io:format("Set Controller Dir to: ~s~n", [Dir]).
